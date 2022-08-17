3cc5405102dcf1786e69247fcb33f884a47115bd
redbuilderOne <forsaken2492@yandex.ru>
2022-08-17 17:26:09 +0300

    first commit

---
+++ avito-iOS-Test-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito-iOS-Test-Task
+//
+//  Created by Дмитрий Скворцов on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
